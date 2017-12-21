b75ca355c3fa2f65af1f03da630e9dac18908d04
Jaime Jazareno III <jaimejazarenoiii@gmail.com>
2017-12-21 18:43:45 +0800

    Initialize finished files

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trafficImage: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var startStopButton: UIButton!
+    
+    var startTimer = Timer()
+    var scoreTimer = Timer()
+    
+    var timerInt = 0
+    var scoreInt = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func startGame(_ sender: Any) {
+        if scoreInt == 0 {
+            timerInt = 3
+            startTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            trafficImage.image = UIImage(named: "trafficLight")
+            scoreLabel.text = String(scoreInt)
+            startStopButton.setTitle("Start", for: UIControlState.normal)
+            startStopButton.isEnabled = false
+        }
+        else {
+            scoreTimer.invalidate()
+            scoreInt = 0
+            startStopButton.setTitle("Restart", for: UIControlState.normal)
+        }
+    }
+    
+    @objc func updateTimer(){
+        timerInt -= 1
+        if timerInt == 2{
+            trafficImage.image = UIImage(named: "trafficLight3")
+        }
+        else if timerInt == 1 {
+            trafficImage.image = UIImage(named: "trafficLight2")
+        }
+        else if timerInt == 0 {
+            trafficImage.image = UIImage(named: "trafficLight1")
+            startTimer.invalidate()
+            scoreTimer = Timer.scheduledTimer(timeInterval: 0.0001, target: self, selector: #selector(updateScoreTimer), userInfo: nil, repeats: true)
+            startStopButton.isEnabled = true
+            startStopButton.setTitle("Stop", for: UIControlState.normal)
+        }
+        
+    }
+    
+    @objc func updateScoreTimer(){
+        scoreInt += 1
+        scoreLabel.text = String(scoreInt)
+    }
+    
 }
 
